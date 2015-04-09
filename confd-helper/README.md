a162c39f613af802c065835d23c80c91c0e28d2e
Li Yi <denverdino@gmail.com>
2015-04-09 14:27:25 +0800

    Initial delivery

--- confd-helper/README.md
+++
@@ -1,7 +0,0 @@
-Confd-helper
-==================================
-
-This project provides the helper function for using Confd within Docker container
-
-  * *exec*: exec command in another Docker container
-  * *kill*: Signal to send to the container
--- confd-helper/test-boot2docker.sh
+++
@@ -1,8 +0,0 @@
-export GOARCH="amd64"
-export GOOS="linux"
-echo "Building ..."
-go build -o confd-helper-linux
-boot2docker ssh "chmod +x $(pwd)/confd-helper-linux"
-echo "Start testing"
-boot2docker ssh "$(pwd)/confd-helper-linux exec --container=5b08d3523b24 -c ls"
-
